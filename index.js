d3c8a2fa2f10b7fb338ebfa3e702268bcaed0ead
TheGBO <gabrielthejota@gmail.com>
2022-02-13 16:16:02 -0300

    added .env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
+require('dotenv').config();
 const socketIo = require('socket.io')(server, {
   cors: {
     origin: '*',
